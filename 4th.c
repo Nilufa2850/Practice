9e5acbc68add9a394c4caec97fa4ac492749ee51
Nilufa Yasmin <116085061+Nilufa2850@users.noreply.github.com>
2023-10-18 19:30:31 +0530

    Create 4th.c

---
+++ 4th.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main ()
+{
+    int a = 5 , b = 6 ;
+    printf("%d %d" , a, b) ;
+    printf("\na=%d and b=%d" , a,b) ;
+    printf("\na=%d and b=%d", b , a) ;
+    printf("\nValue of a is %d and value of b is %d" , a , b) ;
+    printf("\nSum of %d and %d is %d" , a , b , a+b) ;
+    printf("\n%d+%d=%d", a, b , a+b) ;
+    return 0 ;
+}
