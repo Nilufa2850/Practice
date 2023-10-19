1d758925dc0bf885d34692bc18988786fe487048
Nilufa Yasmin <116085061+Nilufa2850@users.noreply.github.com>
2023-10-19 18:12:23 +0530

    Create 17.c

---
+++ 17.c
@@ -0,0 +1,13 @@
+
+#include<stdio.h>
+int main ()
+{
+    int x , y ;
+    printf("Enter a number : ") ;
+    scanf("%d", &x) ;
+
+    y = x / 10 ;
+    printf("without last digit of %d is %d" , x, y ) ;
+
+    return 0 ;
+}
