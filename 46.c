e8a29d9528c462a5e09089ad0b38e147a6cba790
Nilufa Yasmin <116085061+Nilufa2850@users.noreply.github.com>
2023-10-19 21:05:31 +0530

    Create 46.c

---
+++ 46.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main ()
+{
+    int i , n ;
+
+    printf("Enter a number : ") ;
+    scanf ("%d" , &n) ;
+    
+    for (i=2 ; i<=n-1 ; i++)
+    {
+        if (n%i==0)
+            break ;
+    }
+    if (i==n)
+        printf("prime") ;
+    else
+        printf("not prime") ;
+
+    return 0 ;
+}
