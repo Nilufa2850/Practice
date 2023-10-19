29f8b9f91575cd9a5ded7cd861349529bc899c61
Nilufa Yasmin <116085061+Nilufa2850@users.noreply.github.com>
2023-10-19 21:00:31 +0530

    Create 44.c

---
+++ 44.c
@@ -0,0 +1,17 @@
+
+#include<stdio.h>
+int main ()
+{
+    int x , n , sum = 0 ;
+
+    printf("Enter n value : ") ;
+    scanf ("%d" , &n) ;
+    
+    for (x=1 ; x<=n ; x++)
+        sum = sum+(x*x) ;
+
+    printf("sum is %d" , sum) ;
+        
+
+    return 0 ;
+}
