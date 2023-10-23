de378c8ba55a95eb975fba58ef68a6efe6a69a54
Nilufa Yasmin <116085061+Nilufa2850@users.noreply.github.com>
2023-10-23 09:26:59 +0530

    Create 89.c

---
+++ 89.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int fact (int) ;
+
+int main ()
+{
+    int n ;
+    printf("Enter number : ") ;
+    scanf("%d" , &n) ;
+
+    printf("factorial is %d" , fact(n)) ;
+    return 0 ;
+}
+
+int fact (int n)
+{
+    if (n==1 || n==0)
+        return 1 ;
+
+    return n*fact(n-1) ;
+}
