bae719de180632a74caa8079d8726714a4efe060
Nilufa Yasmin <116085061+Nilufa2850@users.noreply.github.com>
2023-10-27 12:06:54 +0530

    Create 141.c

---
+++ 141.c
@@ -0,0 +1,46 @@
+/*8. Write a program in C to print or display an upper triangular matrix.*/
+
+#include<stdio.h>
+int main ()
+{
+    int a[3][3] ;
+
+    int i , j , sum1=0 ;
+
+    printf("Enter 3x3 values of a[] : ") ;
+    for (i=0 ; i<=2 ; i++)
+        for (j=0 ; j<=2 ; j++)
+            scanf("%d" , &a[i][j]) ;
+
+    printf("\nInput Matrix\n") ;
+    for (i=0 ; i<=2 ; i++)
+    {
+        for (j=0 ; j<=2 ; j++)
+            printf("%d   " , a[i][j]) ;
+
+        printf("\n") ;
+    }
+
+    for (i=0 ; i<=2 ; i++)
+    {
+        for (j=0 ; j<=2 ; j++)
+        {
+            if(i+j <= 1)
+                a[i][j] = 00 ;
+        }
+        
+    }
+    printf("\n..........................\n") ;
+    for (i=0 ; i<=2 ; i++)
+    {
+        for (j=0 ; j<=2 ; j++)
+            printf("%d   " , a[i][j]) ;
+
+        printf("\n") ;
+    }
+
+    
+    
+
+    return 0 ;
+}
