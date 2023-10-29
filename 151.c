d37e51dc7bd4f734757c96da448e4f17fbc73c5c
Nilufa Yasmin <116085061+Nilufa2850@users.noreply.github.com>
2023-10-29 08:33:54 +0530

    Update 151.c

--- 151.c
+++ 151.c
@@ -1,1 +1,34 @@
+/*7. Write a program in C to count the total number of alphabets, digits and special
+characters in a string.*/
+
+#include <stdio.h>
+#include<string.h>
+
+int main ()
+{
+    char str [30] ;
+    int i , alphabet=0 , digit=0 , count=0 ;
+
+    printf("Enter a string : ") ;
+    gets(str) ;
+
+    for (i=0 ; str[i] ; i++)
+    {
+        if(str[i]>='a'&& str[i]<='z' || str[i]>='A'&& str[i]<='Z')
+            alphabet++ ;
+        
+        else if (str[i]>='0'&& str[i]<='9')
+            digit++ ;
+        
+        else
+            count++ ;
+    }
+
+    printf("\nTlotal no. of alphabet : %d" , alphabet) ;
+    printf("\nTlotal no. of digit : %d" , digit) ;
+    printf("\nTlotal no. of special character : %d" , count) ;
+    
+    return 0 ;
+}
+
 
