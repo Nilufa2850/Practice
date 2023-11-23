d7cebf19f07d4c472c2b6c3ead66fb555cb3c0f5
Nilufa Yasmin <116085061+Nilufa2850@users.noreply.github.com>
2023-11-23 08:25:32 +0530

    Create sum.c

---
+++ sum.c
@@ -0,0 +1,28 @@
+/*3. Write a program to calculate the sum of n numbers entered by the user using malloc
+and free.*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main ()
+{
+    int n ;
+    printf("Enter size : ") ;
+    scanf("%d" , &n) ;
+
+    int*p = (int*) calloc(n , sizeof(int)) ;
+
+    int i , sum=0 ;
+
+    printf("Enter %d numbers : " , n) ;
+    for (i=0 ; i<n ; i++)
+        scanf("%d" , &p[i]) ;
+
+    for (i=0 ; i<n-1 ; i++)
+    {
+        sum = sum + p[i] ;
+    }
+
+    printf("sum is %d" , sum) ;
+
+}
